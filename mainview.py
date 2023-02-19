57a32c56b2ab9ee4f26fedee1899c3aaea16bd4d
d3miurg <whu19551@gmail.com>
2023-02-19 14:43:06 +0300

    added chat form; preMVP

--- mainview.py
+++ mainview.py
@@ -2,10 +2,16 @@ from app import application
 from app import database
 from flask import request
 from flask import jsonify
-from models import Users
-from models import Chats
+from models import User
+from models import Chat
+from models import Message
+
 
 # нужен полный рефакторинг под REST
+
+
+def construct_responce(reason, **additional): # переделать в декоратор для обработки json
+    pass
 
 
 @application.route('/')
@@ -16,10 +22,10 @@ def index():
 
 @application.route('/register', methods=['GET', 'POST'])
 def register():
-    json_request = request.json
-    new_user = Users(username=json_request['username'],
-                     password=json_request['password'],
-                     slug=json_request['username']) # проверить регистрацию # проверить правильность заполнения формы
+    json_request = request.json # нужно проверить метод запроса и его тело
+    new_user = User(username=json_request['username'],
+                    password=json_request['password'],
+                    slug=json_request['username']) # проверить регистрацию # проверить правильность заполнения формы
     database.session.add(new_user) # убедиться, что база работает
     database.session.commit()
     return jsonify({'status': '1',
@@ -30,8 +36,8 @@ def register():
 def login():
     json_request = request.json # нужно проверить метод запроса и его тело
 
-    recieved_user = Users.query.filter(Users.username == json_request['username']).first() # может быть 2 пользователя с одним именем # пользователь может не существовать
-    if recieved_user.password != json_request['password']:
+    recieved_user = User.query.filter(User.username == json_request['username']).first() # может быть 2 пользователя с одним именем # пользователь может не существовать
+    if (recieved_user.password != json_request['password']):
         return jsonify({'status': '0',
                         'reason': 'invalid password'})
 
@@ -41,7 +47,7 @@ def login():
 
 @application.route('/chat')
 def chatlist():
-    all_chats = Chats.query.all()
+    all_chats = Chat.query.all() # нет проверки валидации
     chat_slugs = []
     for n in all_chats:
         chat_slugs.append(n.slug)
@@ -53,11 +59,31 @@ def chatlist():
 
 @application.route('/chat/<slug>', methods=['GET', 'POST'])
 def chat(slug):
-    print(slug)
+    json_request = request.json # нужно проверить метод запроса и его тело
+    if (json_request['action'] == 'read'):
+        last_messages = Message.query.order_by(Message.id.desc()).count(json_request['value'])
+        last_messages_content = []
+        for n in last_messages:
+            last_messages_content.append(n.content)
 
-    return 'Чаты ещё не готовы'
+        return jsonify({'status': '1',
+                        'reason': 'returned messages',
+                        'last_messages': last_messages_content})
 
+    if (json_request['action'] == 'send'):
+        current_chat = Chat.query.filter(Chat.slug == slug).first()
+        new_message = Message(content=json_request['value'], chat=current_chat.id, author=1)
+        database.session.add(new_message)
+        database.session.commit()
 
+        return jsonify({'status': '1',
+                        'reason': 'sended message to server'})
+
+    return jsonify({'status': '0',
+                    'reason': 'no action given'})
+
+# настройки чата
+# форма создания нового чата
 @application.errorhandler(404)
 def page_not_found(e):
     return ''.join(['Страница не найдена. Убедитесь, что:\n',
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from app import database
 
 
-class Chats_to_users_relation(database.Model):
+class Users_to_chats_relation(database.Model):
     __tablename__ = 'users_to_chats_relations'
     user = database.Column(database.Integer,
                            database.ForeignKey('users.id'),
@@ -9,36 +9,36 @@ class Chats_to_users_relation(database.Model):
     chat = database.Column(database.Integer,
                            database.ForeignKey('chats.id'),
                            primary_key=True)
-    user_relation = database.relationship('Users', back_populates='chats')
-    chat_relation = database.relationship('Chats', back_populates='users')
+    user_relation = database.relationship('User', back_populates='chats')
+    chat_relation = database.relationship('Chat', back_populates='users')
 
 
-class Users(database.Model):
+class User(database.Model):
     __tablename__ = 'users'
     id = database.Column(database.Integer, primary_key=True) # возможно, стоит вынести в отдельный класс
     username = database.Column(database.String(32))
     password = database.Column(database.String(64))
     slug = database.Column(database.String(32)) # дублирование кода, вынести в отдельный класс
-    chats = database.relationship('Chats_to_users_relation',
+    chats = database.relationship('Users_to_chats_relation',
                                   back_populates='user_relation')
 
     def __repr__(self):
         return self.username
 
 
-class Chats(database.Model):
+class Chat(database.Model):
     __tablename__ = 'chats'
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(32))
     users = database.Column(database.Text, database.ForeignKey("users.id"))
     slug = database.Column(database.String(32)) # дублирование
-    users = database.relationship('Chats_to_users_relation',
+    users = database.relationship('Users_to_chats_relation',
                                   back_populates='chat_relation')
 
     def __repr__(self):
         return self.name
 
-class Messages(database.Model):
+class Message(database.Model): # необходимы отношения
     __tablename__ = 'messages'
     id = database.Column(database.Integer, primary_key=True)
     content = database.Column(database.String(2048))
